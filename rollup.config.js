c728a5f6f15146fbae2a68c59cc95880a0d6f074
Joshua Jones <joshuajones2010@gmail.com>
2023-10-03 16:11:38 +1100

    rm trigen

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,15 @@
 import swc from 'rollup-plugin-swc'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import replace from '@rollup/plugin-replace'
+import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import browsersEsm from '@trigen/browserslist-config/browsers-esm'
-import nodeEsm from '@trigen/browserslist-config/node-esm'
-import browsers from '@trigen/browserslist-config/browsers'
-import node from '@trigen/browserslist-config/node'
 import pkg from './package.json'
 
-const extensions = ['.js', '.ts']
-const external = _ => /node_modules/.test(_) && !/@swc\/helpers/.test(_)
-const plugins = targets => [
-  nodeResolve({
-    extensions
-  }),
-  replace({
-    preventAssignment: true,
-    values: {
-      'process.env.NODE_ENV': JSON.stringify(
-        process.env.ROLLUP_WATCH
-          ? 'development'
-          : 'production'
-      )
-    }
-  }),
-  swc({
-    jsc: {
-      parser: {
-        syntax: 'typescript'
-      },
-      externalHelpers: false
-    },
-    env: {
-      targets
-    },
-    module: {
-      type: 'es6'
-    },
-    sourceMaps: true
-  })
-]
-
-export default process.env.NODE_ENV !== 'development' ? [
+export default [
   {
     input: pkg.main,
-    plugins: plugins(browsersEsm.concat(nodeEsm).join(', ')),
-    external,
+    plugins: [
+      typescript(),
+      swc(),
+    ],
     output: {
       file: pkg.publishConfig.module,
       format: 'es',
@@ -54,22 +18,15 @@ export default process.env.NODE_ENV !== 'development' ? [
   },
   {
     input: pkg.main,
-    plugins: plugins(browsers.concat(node).join(', ')),
-    external,
+    plugins: [
+      typescript(),
+      commonjs(),
+      swc(),
+    ],
     output: {
       file: pkg.publishConfig.main,
       format: 'cjs',
       sourcemap: true
     }
   }
-] : {
-  input: pkg.main,
-  plugins: [...plugins(browsersEsm.join(', ')), commonjs()],
-  output: {
-    file: './dist/umd.js',
-    format: 'umd',
-    exports: 'named',
-    name: 'pith',
-    sourcemap: true
-  }
-}
+]
